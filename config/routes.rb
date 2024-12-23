b64649331231a8432eb5595a15e1cf89584c2d10
ford burgess <fburg6824@gmail.com>
2024-12-23 17:21:20 +0100

    added one off pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # pages not associated with a model
   root to: 'pages#home'
+  get '/contact' => 'pages#contact'
+  get '/plants' => 'pages#plants'
 
   resources :products, only: [:index, :show]
 end

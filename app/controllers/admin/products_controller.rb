d72fcbd8bb9b65010352a64eb21281ffed7f0ecd
ford burgess <fburg6824@gmail.com>
2024-12-28 23:20:15 +0100

    layout sort of done

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ProductsController < ApplicationController
-
-
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_product_path(@product.id)
+    end
   end
 
   def show
@@ -11,5 +17,11 @@ class Admin::ProductsController < ApplicationController
 
   def index
     @products = Product.where(type: params[:type])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :price, :stock, :type)
   end
 end

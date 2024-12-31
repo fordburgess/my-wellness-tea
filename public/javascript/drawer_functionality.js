897acbf852144ce21c5304293aabfadaf67e8af3
ford burgess <fburg6824@gmail.com>
2024-12-31 13:31:21 +0100

    drawer menu done

--- public/javascript/drawer_functionality.js
+++ public/javascript/drawer_functionality.js
@@ -2,9 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   let hamburgerMenu = document.getElementById("hamburger-menu");
   let drawerContainer = document.getElementById("drawer-container");
   let drawerMenu = document.getElementById("drawer-menu");
+  let xIcon = document.getElementById("x-icon");
 
   hamburgerMenu.addEventListener('click', () => {
     drawerContainer.style.display = 'block';
     drawerMenu.classList.add('active');
+  })
+
+  xIcon.addEventListener('click', () => {
+    drawerMenu.classList.remove('active');
+    drawerContainer.style.display = 'none';
   })
 });

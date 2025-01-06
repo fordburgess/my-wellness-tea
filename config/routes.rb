e453f20d71f9634a436f5107323fc320aa8f4ab0
ford burgess <fburg6824@gmail.com>
2025-01-06 14:32:30 +0100

    language select now works

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,37 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'carts/show'
-  devise_for :users
+  get '/', to: redirect("/#{I18n.default_locale}")
 
-  # order and cart functionality
-  get 'carts/:id' => "carts#show", as: "cart"
-  delete 'carts/:id' => "carts#destroy"
+  scope "/:locale", locale: /fr|de|it|en/ do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/new'
+    get 'carts/show'
+    devise_for :users
 
-  post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
-  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
-  post 'line_items' => "line_items#create"
-  get 'line_items/:id' => "line_items#show", as: "line_item"
-  delete 'line_items/:id' => "line_items#destroy"
+    # order and cart functionality
+    get 'carts/:id' => "carts#show", as: "cart"
+    delete 'carts/:id' => "carts#destroy"
 
-  resources :orders
+    post 'line_items/:id/add' => "line_items#add_quantity", as: "line_item_add"
+    post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "line_item_reduce"
+    post 'line_items' => "line_items#create"
+    get 'line_items/:id' => "line_items#show", as: "line_item"
+    delete 'line_items/:id' => "line_items#destroy"
 
-  # pages not associated with a model
-  root to: 'pages#home'
-  get '/contact' => 'pages#contact'
-  get '/plants' => 'pages#plants'
+    resources :orders
 
-  # admin routes
-  namespace :admin do
-    root to: 'admin#dashboard'
-    # resources :products
-    resources :teas
+    # pages not associated with a model
+    root to: 'pages#home'
+    get '/contact' => 'pages#contact'
+    get '/plants' => 'pages#plants'
+
+    # admin routes
+    namespace :admin do
+      root to: 'admin#dashboard'
+      # resources :products
+      resources :teas
+    end
+
+    resources :teas, only: [:index, :show]
   end
-
-  resources :teas, only: [:index, :show]
 end

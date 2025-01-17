dcf2263f13bdd4e41cc938aa461f8fa437e1a69d
ford burgess <fburg6824@gmail.com>
2025-01-17 14:55:48 +0000

    before adding stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_16_145519) do
+ActiveRecord::Schema.define(version: 2025_01_16_171511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2025_01_16_145519) do
     t.text "street_address"
     t.string "post_code"
     t.string "country"
+    t.string "city"
   end
 
   create_table "products", force: :cascade do |t|

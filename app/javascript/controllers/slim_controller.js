9ed4d3debb35d5101a69ca4ac45a70dde5cdb960
ford burgess <fburg6824@gmail.com>
2025-01-19 17:50:34 +0000

    styling for plant selection done

--- app/javascript/controllers/slim_controller.js
+++ app/javascript/controllers/slim_controller.js
@@ -4,9 +4,12 @@ import SlimSelect from 'slim-select'
 // Connects to data-controller="slim"
 export default class extends Controller {
   connect() {
-
-    new SlimSelect({
-      select: this.element
-    })
+    this.slimSelect = new SlimSelect({
+      select: this.element,
+      showSearch: true, // Optional
+      settings: {
+        addToBody: false, // Prevent Slim Select from appending to <body>
+      },
+    });
   }
 }

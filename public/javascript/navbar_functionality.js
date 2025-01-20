ba1e56e53476084f854372a4213131098a9abefe
ford burgess <fburg6824@gmail.com>
2025-01-20 12:39:37 +0000

    will come back to lang select later

--- public/javascript/navbar_functionality.js
+++ public/javascript/navbar_functionality.js
@@ -1,29 +1,17 @@
 document.addEventListener("turbo:load", () => {
   let langMenuButton = document.getElementById("lang-menu-button");
-  let langMenuDropdown = document.getElementById("lang-menu-dropdown");
-  let langMenuButtonMobile = document.getElementById("lang-menu-button-mobile");
-  let langMenuDropdownMobile = document.getElementById("lang-menu-dropdown-mobile");
+  let langMenuDropdown = document.querySelector(".lang-menu-dropdown");
 
+  console.log(langMenuDropdown)
   langMenuButton.addEventListener("click", (e) => {
     e.stopPropagation();
-    langMenuDropdown.style.display = langMenuDropdown.style.display == 'block' ? 'none' : 'block';
-  })
-
-  langMenuButtonMobile.addEventListener("click", (e) => {
-    e.stopPropagation();
-    langMenuDropdownMobile.style.display = langMenuDropdownMobile.style.display == 'block' ? 'none' : 'block';
-    console.log(langMenuDropdownMobile.style)
+    langMenuDropdown.classList.toggle('active-dropdown')
+    // langMenuDropdown.style.display = langMenuDropdown.style.display == 'block' ? 'none' : 'block';
   })
 
   document.addEventListener('click', (e) => {
     if (!langMenuButton.contains(e.target) && !langMenuDropdown.contains(e.target)) {
-      langMenuDropdown.style.display = 'none';
-    }
-  })
-
-  document.addEventListener('click', (e) => {
-    if (!langMenuButtonMobile.contains(e.target) && !langMenuDropdownMobile.contains(e.target)) {
-      langMenuDropdownMobile.style.display = 'none';
+      langMenuDropdown.classList.remove('active-dropdown')
     }
   })
 })

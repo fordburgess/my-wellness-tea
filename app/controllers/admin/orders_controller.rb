e8dd51ea2ca1f3b68a140385c5317f0638c62ea1
ford burgess <fburg6824@gmail.com>
2025-03-04 11:21:50 +0000

    started order table

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  Stripe.api_key = "sk_test_51P5XAnHLVVBFu1EItUhXzrhTjlJo3myCxKZkvMKGOONeoK7qYBhgGYuRskMHm7GYxE5IP6FczjHzGa9GmqZKDazc00abzIMZkD"
+
   def index
+#     payments = Stripe::PaymentIntent.list()
+#
+#     payments.data.each do |payment|
+#       id = payment.id
+#       charge = Stripe::Charge.retrieve(payment.latest_charge)
+#       puts charge
+#     end
   end
 end

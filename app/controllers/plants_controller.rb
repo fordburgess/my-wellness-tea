ed371943d04bef8624e05ae104100b47277a72e4
ford burgess <fburg6824@gmail.com>
2025-03-12 13:59:16 +0000

    plant index page almost done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,16 +2,17 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.all
 
-    @plants_wellbeing = Tea.joins(:tea_detail).where(tea_detail: { category: 'Bien-être' })
+    @teas_wellbeing = Tea.joins(:tea_detail).where(tea_detail: { category: 'Bien-être' })
+    @plants_wellbeing = @teas_wellbeing.flat_map(&:plants).uniq
 
     @teas_wellbeing_women = Tea.joins(:tea_detail).where(tea_detail: { category: 'Bien-être chez la femme' })
     @plants_wellbeing_women = @teas_wellbeing_women.flat_map(&:plants).uniq
 
+    @teas_winter = Tea.joins(:tea_detail).where(tea_detail: { category: 'Hiver' })
+    @plants_winter = @teas_winter.flat_map(&:plants).uniq
 
-
-    @plants_winter = Tea.joins(:tea_detail).where(tea_detail: { category: 'Hiver' })
-
-    @plants_digestion = Tea.joins(:tea_detail).where(tea_detail: { category: 'Bien-être digestif' })
+    @teas_digestion = Tea.joins(:tea_detail).where(tea_detail: { category: 'Bien-être digestif' })
+    @plants_digestion = @teas_digestion.flat_map(&:plants).uniq
   end
 
   def show

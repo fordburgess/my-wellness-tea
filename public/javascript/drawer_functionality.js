821eef9314f42e2414587db69fb6c457e4401118
ford burgess <fburg6824@gmail.com>
2025-03-17 19:49:11 +0000

    added cloudinary folder

--- public/javascript/drawer_functionality.js
+++ public/javascript/drawer_functionality.js
@@ -30,26 +30,4 @@ document.addEventListener("turbo:load", () => {
     cartDrawerContainer.style.display = 'block';
     cartDrawerMenu.classList.add('active');
   })
-
-
-
-
-
-
-  let langMenuButton = document.getElementById("lang-menu-button");
-  let langMenuDropdown = document.getElementById("lang-menu-dropdown");
-
-  langMenuButton.addEventListener("click", (e) => {
-    e.stopPropagation();
-    console.log("Lang menu clicked")
-
-    langMenuDropdown.classList.toggle('active-dropdown')
-    // langMenuDropdown.style.display = langMenuDropdown.style.display == 'block' ? 'none' : 'block';
-  })
-
-  document.addEventListener('click', (e) => {
-    if (!langMenuButton.contains(e.target) && !langMenuDropdown.contains(e.target)) {
-      langMenuDropdown.classList.remove('active-dropdown')
-    }
-  })
 });

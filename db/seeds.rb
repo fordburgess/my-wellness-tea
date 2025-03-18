639dade74a22a8c61fcde76bdf193ae88b47d2e9
ford burgess <fburg6824@gmail.com>
2025-03-18 14:48:52 +0000

    added dean to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
+User.delete_all
+
 puts "Creating Users"
 User.create(email: "fordburgess1@gmail.com", password: 'password123', admin: true);
+
+User.create(email: "deantresadern@gmail.com", password: 'password123', admin: true);
 
 puts "Finished Seeding"

ac561d7a41200f18bc3dbfaf205440fd02ae7486
ford burgess <fburg6824@gmail.com>
2025-03-19 11:09:18 +0000

    cannot submit form unless all fields are filled out

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,9 @@
 class Tea < Product
   has_one :tea_detail, foreign_key: "product_id"
   accepts_nested_attributes_for :tea_detail
+
+  validates :title, :price, :stock, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :images, presence: true
+  validates_associated :tea_detail
 end
